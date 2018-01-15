f874d67abe8b7430a23b666222ff4a59f873b42b
emacslisp <java.gdb@gmail.com>
2018-01-15 23:27:47 +1100

    add radButtonElement

--- FileExplorer/FileExplorer/Form1.Designer.cs
+++ FileExplorer/FileExplorer/Form1.Designer.cs
@@ -33,6 +33,12 @@
             this.ribbonTab2 = new Telerik.WinControls.UI.RibbonTab();
             this.ribbonTab3 = new Telerik.WinControls.UI.RibbonTab();
             this.ribbonTab4 = new Telerik.WinControls.UI.RibbonTab();
+            this.radRibbonBarGroup1 = new Telerik.WinControls.UI.RadRibbonBarGroup();
+            this.radDropDownButtonElement1 = new Telerik.WinControls.UI.RadDropDownButtonElement();
+            this.radRibbonBarButtonGroup1 = new Telerik.WinControls.UI.RadRibbonBarButtonGroup();
+            this.radButtonElement1 = new Telerik.WinControls.UI.RadButtonElement();
+            this.radButtonElement2 = new Telerik.WinControls.UI.RadButtonElement();
+            this.radButtonElement3 = new Telerik.WinControls.UI.RadButtonElement();
             ((System.ComponentModel.ISupportInitialize)(this.radRibbonBar1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
             this.SuspendLayout();
@@ -46,7 +52,7 @@
             this.ribbonTab4});
             this.radRibbonBar1.Location = new System.Drawing.Point(0, 0);
             this.radRibbonBar1.Name = "radRibbonBar1";
-            this.radRibbonBar1.Size = new System.Drawing.Size(768, 148);
+            this.radRibbonBar1.Size = new System.Drawing.Size(768, 162);
             this.radRibbonBar1.TabIndex = 0;
             this.radRibbonBar1.Text = "Form1";
             // 
@@ -54,6 +60,9 @@
             // 
             this.ribbonTab1.AccessibleDescription = "Home";
             this.ribbonTab1.AccessibleName = "Home";
+            this.ribbonTab1.IsSelected = true;
+            this.ribbonTab1.Items.AddRange(new Telerik.WinControls.RadItem[] {
+            this.radRibbonBarGroup1});
             this.ribbonTab1.Name = "ribbonTab1";
             this.ribbonTab1.Text = "Home";
             // 
@@ -78,6 +87,62 @@
             this.ribbonTab4.Image = global::FileExplorer.Properties.Resources._1_copy_as_path;
             this.ribbonTab4.Name = "ribbonTab4";
             this.ribbonTab4.Text = "Themes";
+            // 
+            // radRibbonBarGroup1
+            // 
+            this.radRibbonBarGroup1.AccessibleDescription = "Clipboard";
+            this.radRibbonBarGroup1.AccessibleName = "Clipboard";
+            this.radRibbonBarGroup1.Items.AddRange(new Telerik.WinControls.RadItem[] {
+            this.radDropDownButtonElement1,
+            this.radRibbonBarButtonGroup1});
+            this.radRibbonBarGroup1.Name = "radRibbonBarGroup1";
+            this.radRibbonBarGroup1.Text = "Clipboard";
+            // 
+            // radDropDownButtonElement1
+            // 
+            this.radDropDownButtonElement1.AccessibleDescription = "Paste";
+            this.radDropDownButtonElement1.AccessibleName = "Paste";
+            this.radDropDownButtonElement1.ArrowButtonMinSize = new System.Drawing.Size(12, 12);
+            this.radDropDownButtonElement1.ArrowPosition = Telerik.WinControls.UI.DropDownButtonArrowPosition.Bottom;
+            this.radDropDownButtonElement1.DropDownDirection = Telerik.WinControls.UI.RadDirection.Down;
+            this.radDropDownButtonElement1.ExpandArrowButton = false;
+            this.radDropDownButtonElement1.Image = global::FileExplorer.Properties.Resources._1_paste;
+            this.radDropDownButtonElement1.ImageAlignment = System.Drawing.ContentAlignment.MiddleCenter;
+            this.radDropDownButtonElement1.Name = "radDropDownButtonElement1";
+            this.radDropDownButtonElement1.Text = "Paste";
+            this.radDropDownButtonElement1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // radRibbonBarButtonGroup1
+            // 
+            this.radRibbonBarButtonGroup1.AccessibleDescription = "radRibbonBarButtonGroup1";
+            this.radRibbonBarButtonGroup1.AccessibleName = "radRibbonBarButtonGroup1";
+            this.radRibbonBarButtonGroup1.Items.AddRange(new Telerik.WinControls.RadItem[] {
+            this.radButtonElement1,
+            this.radButtonElement2,
+            this.radButtonElement3});
+            this.radRibbonBarButtonGroup1.Name = "radRibbonBarButtonGroup1";
+            this.radRibbonBarButtonGroup1.Text = "radRibbonBarButtonGroup1";
+            // 
+            // radButtonElement1
+            // 
+            this.radButtonElement1.AccessibleDescription = "radButtonElement1";
+            this.radButtonElement1.AccessibleName = "radButtonElement1";
+            this.radButtonElement1.Name = "radButtonElement1";
+            this.radButtonElement1.Text = "radButtonElement1";
+            // 
+            // radButtonElement2
+            // 
+            this.radButtonElement2.AccessibleDescription = "radButtonElement2";
+            this.radButtonElement2.AccessibleName = "radButtonElement2";
+            this.radButtonElement2.Name = "radButtonElement2";
+            this.radButtonElement2.Text = "radButtonElement2";
+            // 
+            // radButtonElement3
+            // 
+            this.radButtonElement3.AccessibleDescription = "radButtonElement3";
+            this.radButtonElement3.AccessibleName = "radButtonElement3";
+            this.radButtonElement3.Name = "radButtonElement3";
+            this.radButtonElement3.Text = "radButtonElement3";
             // 
             // Form1
             // 
@@ -105,6 +170,12 @@
         private Telerik.WinControls.UI.RibbonTab ribbonTab2;
         private Telerik.WinControls.UI.RibbonTab ribbonTab3;
         private Telerik.WinControls.UI.RibbonTab ribbonTab4;
+        private Telerik.WinControls.UI.RadRibbonBarGroup radRibbonBarGroup1;
+        private Telerik.WinControls.UI.RadDropDownButtonElement radDropDownButtonElement1;
+        private Telerik.WinControls.UI.RadRibbonBarButtonGroup radRibbonBarButtonGroup1;
+        private Telerik.WinControls.UI.RadButtonElement radButtonElement1;
+        private Telerik.WinControls.UI.RadButtonElement radButtonElement2;
+        private Telerik.WinControls.UI.RadButtonElement radButtonElement3;
     }
 }
 
